e2c78f2dd6969a050a76fad6c81380a6ebfc0949
hilmyy17 <153361845+hilmyy17@users.noreply.github.com>
2024-09-09 11:44:11 +0700

    Add files via upload

---
+++ latihan_php/profile.php
@@ -0,0 +1,10 @@
+<?php 
+$nama = "Mochammad hilmy miftahurrahman "; # variabel nama bernilai mochammad hilmy
+$umur =16 ; # variabel umur bernilai 16
+$sekolah = "SMKN 2"; # variabel sekolah bernilai SMKN 2
+$citacita = "front-end Developer"; # variabel citacita bernilai front end 
+echo "Nama: $nama <br>"; # menampilkan teks Nama dalam variabel $ nama
+echo "Umur: $umur tahun <br>"; # menampilkaan teks Umur dalam variabel $ umur 
+echo "Sekolah:$sekolah <br>"; # menampilkan teks Sekolah dalam variabel $ sekolah
+echo "Citacita: $citacita<br>"; # menampilkan teks Citacita dalam variabel $ citacita
+?>
